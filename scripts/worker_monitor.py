00d40dbc123bb27b5bfbedb9c7be091a4bde3929
Z User <z@container>
2026-09-16 20:57:21 +0000

    lead session B: dep-014 re-land (chat c96f689d) + packet base refresh to ffe5a20 + monitor retarget

--- scripts/worker_monitor.py
+++ scripts/worker_monitor.py
@@ -18,7 +18,7 @@ import channel
 POLL_S = 300
 WATCH = {
     "2442b7bd-d8c7-43de-a593-141eca8c15c0": "dep-011",
-    "687ad7c5-f2f8-4596-9a6b-0e316d8a7dba": "dep-014",
+    "c96f689d-c234-45a8-9321-0d29ee2c9c8f": "dep-014",
 }
 LOG = "/home/z/replay2/scripts/logs/worker_monitor.log"
 
